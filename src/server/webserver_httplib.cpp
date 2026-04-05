1171d669d5889614373badac6d933212b18b1ed8
MirageMRC <wary1540458457@gmail.com>
2026-04-05 16:43:05 +0800

    fix: 更新请求头以防止部分站点阻止

--- src/server/webserver_httplib.cpp
+++ src/server/webserver_httplib.cpp
@@ -146,7 +146,7 @@ int WebServer::start_web_server_multi(listener_args *args)
         writeLog(0, "handle_cmd:    " + req.method + " handle_uri:    " + req.target, LOG_LEVEL_VERBOSE);
         writeLog(0, "handle_header: " + dump(req.headers), LOG_LEVEL_VERBOSE);
 
-        if (req.has_header("SubConverter-Request"))
+        if (req.has_header("MRConverter-Request"))
         {
             res.status = 500;
             res.set_content("Loop request detected!", "text/plain");
